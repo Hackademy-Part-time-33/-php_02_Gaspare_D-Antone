40196bfc9bbedba08d717a7cc454ce387d1fde0a
gaspare d'antone <dantogas@gmail.com>
2024-04-18 20:41:01 +0200

    save

--- index.php
+++ index.php
@@ -50,25 +50,25 @@ foreach($users as $user){
 // Traccia 3:
 // Scrivere un programma che stampi in console tutti i numeri da uno a cento. Se il numero è multiplo di 3 stampare “PHP” al posto del numero; se multiplo di 5 stampare “JAVASCRIPT”; se multiplo di 3 e 5 contemporaneamente deve stampare “HACKADEMY".
 
-// for ($i = 1; $i <= 100; $i++){
+for ($i = 1; $i <= 100; $i++){
 
-//     if ($i %15 == 0) {
-//         echo "HACKADEMY\n";
+    if ($i %15 == 0) {
+        echo "HACKADEMY\n";
     
-//     }elseif($i % 3 == 0){
+    }elseif($i % 3 == 0){
 
-//         echo "PHP\n";
-//     }elseif ($i % 5 == 0){
+        echo "PHP\n";
+    }elseif ($i % 5 == 0){
 
-//         echo "JAVASCRIPT\n";
-//     }
-//     else{
-//         echo $i . "\n";
+        echo "JAVASCRIPT\n";
+    }
+    else{
+        echo $i . "\n";
 
-//     };
+    };
 
         
-// };
+};
 
 
 
